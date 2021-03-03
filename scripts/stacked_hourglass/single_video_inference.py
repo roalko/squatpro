c1c9bd4a19d88822f6146d77b6b88a5d39601f9f
hyunjong song <ssogary00@hotmail.com>
2021-03-03 09:43:50 -0500

    updated

--- scripts/stacked_hourglass/single_video_inference.py
+++ scripts/stacked_hourglass/single_video_inference.py
@@ -3,6 +3,8 @@ import sys
 import os
 import numpy as np
 import cv2
+import pandas as pd
+import csv
 
 from hourglass import HourglassNet
 from mpii_datagen import MPIIDataGen
@@ -31,6 +33,15 @@ def inference(model_json, model_weights, img, threshold = 0.05, num_stack = 2, n
             conf = 0.9
         mkps.append((4*scale[1]*keypoint[0], 4*scale[0]*keypoint[1], conf))
 
+    coor_dict = {}
+    key = 0
+    for i in mkps:
+        x,y = i[0],i[1]
+        coor_dict[key] = x
+        key+=1
+        coor_dict[key] = y
+        key+=1
+
 
     #im = cv2.imread(img)
     #x, y = int(mkps[0][0]), int(mkps[0][1])
@@ -44,25 +55,43 @@ def inference(model_json, model_weights, img, threshold = 0.05, num_stack = 2, n
     frame = render_connections(cv2.imread(img), mkps, kp_keys, connections)
     frame = render_joints(frame, mkps, threshold)
 
-    return frame
+    return frame , coor_dict
 
 
-video_path = '../../raw_data/vid1.MOV'
-model_json = '../../raw_data/hg_s2_b1/net_arch.json'
-model_weights = '../../raw_data/hg_s2_b1/weights_epoch96.h5'
+model_json = '../../raw_data/trained_models/hg_s2_b1/net_arch.json'
+model_weights = '../../raw_data/trained_models/hg_s2_b1/weights_epoch96.h5'
 connections = [(0, 1), (1, 2), (2, 6), (3, 6), (3, 4), (4, 5), (6, 7), (7, 8), (8, 9), (8, 12), (12, 11), (11, 10), (8, 13), (13, 14), (14, 15)]
 
-cap = cv2.VideoCapture(video_path)
 
-if cap.isOpened() is False:
-    print("Error opening video stream or file")
 
-while cap.isOpened():
-    ret_val, input_image = cap.read()
-    cv2.imwrite('../../raw_data/out.jpg', input_image)
+for vid_num in range(27,101):
 
-    frame = inference(model_json = model_json, model_weights = model_weights, img = '../../raw_data/out.jpg')
+    video_path = f'../../raw_data/{vid_num}.mov'
+    cap = cv2.VideoCapture(video_path)
 
-    cv2.imshow('output', frame)
-    cv2.waitKey(1)
+    if cap.isOpened() is False:
+        print("Error opening video stream or file")
+
+    coordinates = []
+    while cap.isOpened():
+        ret_val, input_image = cap.read()
+        if ret_val ==True:
+
+            cv2.imwrite('../../raw_data/out.jpg', input_image)
+
+            frame, coor_dict = inference(model_json = model_json, model_weights = model_weights, img = '../../raw_data/out.jpg')
+
+            coordinates.append(coor_dict)
+
+
+        else:
+            break
+
+    with open(f'{vid_num}.csv', 'w') as output_file:
+        fc = csv.DictWriter(output_file,
+                            fieldnames=coordinates[0].keys(),
+
+                           )
+        fc.writeheader()
+        fc.writerows(coordinates)
 
