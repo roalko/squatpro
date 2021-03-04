e8bdd480b47a9c166eefa05eb7fb142b790e6178
viralbthakar <viralbthakar@gmail.com>
2021-03-04 18:05:29 -0500

    Update

--- scripts/stacked_hourglass/single_video_inference.py
+++ scripts/stacked_hourglass/single_video_inference.py
@@ -5,22 +5,22 @@ import numpy as np
 import cv2
 import pandas as pd
 import csv
-
+import skvideo.io
 from hourglass import HourglassNet
 from mpii_datagen import MPIIDataGen
 from data_process import *
 
 
-def load_model(model_json, model_weights, img, threshold = 0.05, num_stack = 2, num_class = 16, tiny = False):
+def load_model(model_json, model_weights, img, threshold = 0.05, num_stack = 4, num_class = 16, tiny = False):
     if tiny:
         xnet = HourglassNet(num_classes = num_class, num_stacks = num_stack, num_channels = 128, inres = (192, 192), outres = (48, 48))
     else:
-        xnet = HourglassNet(num_classes = num_class, num_stacks = num_stack, num_channels = 256, inres = (256, 256), outres = (64, 64))
+        xnet = HourglassNet(num_classes = num_class, num_stacks = num_stack, num_channels = 256, inres = (256, 256), outres = (256, 256))
 
     xnet.load_model(model_json, model_weights)
     return xnet
 
-def inference(xnet, model_json, model_weights, img, threshold = 0.05, num_stack = 2, num_class = 16, tiny = False):
+def inference(xnet, model_json, model_weights, img, threshold = 0.2, num_stack = 4, num_class = 16, tiny = False):
     out, scale = xnet.inference_file(img)
     keypoints = post_process_heatmap(out[0, :, :, :])
     #ignore_kps = ['plevis', 'thorax', 'head_top']
@@ -32,17 +32,23 @@ def inference(xnet, model_json, model_weights, img, threshold = 0.05, num_stack 
         if kp_keys[i] in ignore_kps:
             conf = 0.0
         else:
-            conf = 0.9
+            conf = keypoint[2]
         mkps.append((4*scale[1]*keypoint[0], 4*scale[0]*keypoint[1], conf))
 
     coor_dict = {}
     key = 0
     for i in mkps:
         x,y = i[0],i[1]
-        coor_dict[key] = x
-        key+=1
-        coor_dict[key] = y
-        key+=1
+        if i[2] >= threshold:
+            coor_dict[key] = x
+            key+=1
+            coor_dict[key] = y
+            key+=1
+        else:
+            coor_dict[key] = 0
+            key+=1
+            coor_dict[key] = 0
+            key+=1
 
 
     #im = cv2.imread(img)
@@ -64,25 +70,24 @@ model_weights = '../../raw_data/trained_model/hg_s2_b1/weights_epoch96.h5'
 connections = [(0, 1), (1, 2), (2, 6), (3, 6), (3, 4), (4, 5), (6, 7), (7, 8), (8, 9), (8, 12), (12, 11), (11, 10), (8, 13), (13, 14), (14, 15)]
 
 
-for vid_num in range(100,101):
+for vid_num in range(1,101):
     video_path = f'../../raw_data/vids/{vid_num}.mov'
-    cap = cv2.VideoCapture(video_path)
-
-    if cap.isOpened() is False:
-        print("Error opening video stream or file")
-
+    cap=skvideo.io.vread(video_path)
 
     xnet = load_model(model_json = model_json, model_weights = model_weights, img = '../../raw_data/out.jpg')
 
     coordinates = []
-    while cap.isOpened():
-        ret_val, input_image = cap.read()
-        if ret_val ==True:
-            cv2.imwrite('../../raw_data/out.jpg', input_image)
-            frame, coor_dict = inference(xnet=xnet, model_json = model_json, model_weights = model_weights, img = '../../raw_data/out.jpg')
-            coordinates.append(coor_dict)
-        else:
-            break
+    for frameId, frame in enumerate(cap):
+        cv2.imwrite('../../raw_data/out.jpg', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+        frame, coor_dict = inference(xnet=xnet, model_json = model_json, model_weights = model_weights, img = '../../raw_data/out.jpg')
+        coordinates.append(coor_dict)
+
+        scale_percent = 0.6
+        width = int(frame.shape[1] * scale_percent)
+        height = int(frame.shape[0] * scale_percent)
+        dim = (width, height)
+        cv2.imshow('output', cv2.resize(frame, dim))
+        cv2.waitKey(1)
 
     print(f'../../raw_data/CSV_Files/{vid_num}.csv')
     with open(f'../../raw_data/CSV_Files/{vid_num}.csv', 'w') as output_file:
